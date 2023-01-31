5d2ee67d3020f529749434bbcf8c88af47538757
1CornBoy <sacul99999@gmail.com>
2023-01-31 14:39:14 -0600

    commit!!!!!!

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -3,10 +3,46 @@
 // You should delete this comment and replace it with your class
 // header comment.
 
+import java.io.PrintStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class QuestionsGame {
     // Your code here
 
+
     private static class QuestionNode {
-        // Your code here
+        public final String q;
+        public QuestionNode yes;
+        public QuestionNode no;
+
+        public QuestionNode(String question){
+            q = question;
+            this.yes = null;
+            this.no = null;
+        }
+        public QuestionNode(String question, QuestionNode yes, QuestionNode no){
+            q = question;
+            this.yes = left;
+            this.no = right;
+        }
     }
+    public QuestionsGame(String object){
+        QuestionsGame a = new QuestionsGame(object);
+    }
+
+    public QuestionsGame(String input){
+
+    }
+    public void saveQuestions(PrintStream output){
+
+    }
+
+    public void play(){
+
+    }
+
+
 }
