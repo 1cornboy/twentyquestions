71315bc653dee2876aec0172c8eae39e82adf363
madcat273 <Shajus2006@gmail.com>
2023-02-01 13:57:38 -0600

    Update QuestionsGame.java

--- src/QuestionsGame.java
+++ src/QuestionsGame.java
@@ -1,48 +1,15 @@
 // This is a starter file for QuestionsGame.
 //
-// You should delete this comment and replace it with your class
+// QuestionsGame.java
 // header comment.
 
-import java.io.PrintStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.Scanner;
-
 public class QuestionsGame {
-    // Your code here
-
+    // brain cancer
+	
 
     private static class QuestionNode {
-        public final String q;
-        public QuestionNode yes;
-        public QuestionNode no;
-
-        public QuestionNode(String question){
-            q = question;
-            this.yes = null;
-            this.no = null;
-        }
-        public QuestionNode(String question, QuestionNode yes, QuestionNode no){
-            q = question;
-            this.yes = left;
-            this.no = right;
-        }
+        // more cancer
+    	QuestionNode =
+    	
     }
-    public QuestionsGame(String object){
-        QuestionsGame a = new QuestionsGame(object);
-    }
-
-    public QuestionsGame(String input){
-
-    }
-    public void saveQuestions(PrintStream output){
-
-    }
-
-    public void play(){
-
-    }
-
-
 }
